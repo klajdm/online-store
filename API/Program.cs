b5ccf3bd74226a138824f327f41210bd8f850a3d
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-25 18:12:52 +0200

    seed the db

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,19 @@ builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "Problem migrating data");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
