03c065fdaa0dd7da09f0f3c1ac8a36436199d74a
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-25 17:43:37 +0200

    added dbcontext and first migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite(
+builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 var app = builder.Build();
 
@@ -14,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.Run();
 
