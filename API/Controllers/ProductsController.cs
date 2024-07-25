7150492f97d8823a68b7c3bcd413170c0e5e1851
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-25 18:32:38 +0200

    use async methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,18 +17,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = _context.Products.ToList();
+            return await _context.Products.ToListAsync();
 
-            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
 
-            return _context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
