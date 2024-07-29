720645df083f81a49d1a304bd407ccbc5d345785
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-29 20:09:49 +0200

    updated cors policy

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
     });
 });
 
