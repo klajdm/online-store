8ac2025bee5be4a6554c224cbbf58e865a84a669
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-30 00:07:46 +0200

    added checkout component

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -8,6 +8,7 @@ import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
 import ContactPage from "../../features/contact/ContactPage";
 import BasketPage from "../../features/basket/BasketPage";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
 
 export const router = createBrowserRouter([
   {
@@ -22,7 +23,7 @@ export const router = createBrowserRouter([
       { path: "/server-error", element: <ServerError /> },
       { path: "/not-found", element: <NotFound /> },
       { path: "/basket", element: <BasketPage /> },
-      // { path: '/checkout', element: <CheckoutPage /> },
+      { path: "/checkout", element: <CheckoutPage /> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
   },
