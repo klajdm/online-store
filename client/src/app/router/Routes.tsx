aa320d1368b6c79478dfda1d3851dd49f498c9a8
Klajdi Murataj <klajdimurati3@gmail.com>
2024-08-06 22:25:59 +0200

    added routes and register component

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -9,12 +9,19 @@ import App from "../layout/App";
 import ContactPage from "../../features/contact/ContactPage";
 import BasketPage from "../../features/basket/BasketPage";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
+import RequireAuth from "./RequireAuth";
+import Register from "../../features/account/Register";
+import Login from "../../features/account/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        element: <RequireAuth />,
+        children: [{ path: "/checkout", element: <CheckoutPage /> }],
+      },
       { path: "", element: <HomePage /> },
       { path: "catalog", element: <Catalog /> },
       { path: "catalog/:id", element: <ProductDetails /> },
@@ -23,7 +30,8 @@ export const router = createBrowserRouter([
       { path: "/server-error", element: <ServerError /> },
       { path: "/not-found", element: <NotFound /> },
       { path: "/basket", element: <BasketPage /> },
-      { path: "/checkout", element: <CheckoutPage /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
   },
